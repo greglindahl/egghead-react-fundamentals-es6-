565f5653a3b4ed15773d66195bdc5c02ab1ce43b
greglindahl <greglindahl410@gmail.com>
2016-10-11 16:07:24 -0600

    Initial commit

---
+++ lessons/01-intro/GregDo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// A class component can have state
+class App extends React.Component {
+  render () {
+    return <div>Hi</div>
+  }
+};
+
+// A stateless functional component will not have state
+// const App = () => <h1>Hello Eggheads</h1>
+
+export default App;
